8d6b6c26bf863408e8d01d48bfe4e7fb6dc776b8
macrusso <mac.rusek1@gmail.com>
2018-10-20 15:40:16 +0100

    user auth

--- src/Entities/Auth/index.ts
+++ src/Entities/Auth/index.ts
@@ -1,6 +1,6 @@
 import * as authConstants from './authConstants';
 import * as authActions from './authActions';
 import * as authSagas from './authSagas';
-import authReducer, { ICommentsState } from './authReducer';
+import authReducer, { IAuthState } from './authReducer';
 
-export { authSagas, ICommentsState, authActions, authReducer, authConstants };
+export { authSagas, IAuthState, authActions, authReducer, authConstants };
