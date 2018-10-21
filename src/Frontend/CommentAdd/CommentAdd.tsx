426229825c97e4681264cfc1223f6c2bc35c3266
macrusso <mac.rusek1@gmail.com>
2018-10-21 20:55:20 +0100

    post + comment add styling

--- src/Frontend/CommentAdd/CommentAdd.tsx
+++ src/Frontend/CommentAdd/CommentAdd.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Formik } from 'formik';
 import { ICommentPartial } from '../../Entities';
+import { TextField, Button, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const StyledForm = styled.form`
+  margin-bottom: 20px;
+  width: 500px;
+`;
 
 interface ICommentAddProps {
   userId: string;
@@ -11,8 +24,10 @@ interface ICommentAddProps {
 const CommentAdd: React.SFC<ICommentAddProps> = props => {
   const { userId, addComment, selectedPostId } = props;
   return (
-    <div>
-      <h1>Create a comment!</h1>
+    <>
+      <Typography variant="h6" align="center">
+        Add your comment
+      </Typography>
       {selectedPostId && (
         <Formik
           initialValues={{ body: '' }}
@@ -21,20 +36,31 @@ const CommentAdd: React.SFC<ICommentAddProps> = props => {
           }}
         >
           {({ values, handleChange, handleBlur, handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <input
-                type="body"
+            <StyledForm onSubmit={handleSubmit}>
+              <TextField
+                id="body"
+                label="Comment"
                 name="body"
+                fullWidth={true}
+                margin="normal"
+                variant="outlined"
                 onChange={handleChange}
-                onBlur={handleBlur}
                 value={values.body}
+                onBlur={handleBlur}
+                InputLabelProps={{
+                  shrink: true,
+                }}
               />
-              <button type="submit">Submit</button>
-            </form>
+              <ButtonGroup>
+                <Button variant="contained" type="submit" color="primary">
+                  Submit
+                </Button>
+              </ButtonGroup>
+            </StyledForm>
           )}
         </Formik>
       )}
-    </div>
+    </>
   );
 };
 
--- src/Frontend/PostAdd/PostAdd.tsx
+++ src/Frontend/PostAdd/PostAdd.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Formik } from 'formik';
 import { IPostPartial } from '../../Entities';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Typography } from '@material-ui/core';
 import styled from 'styled-components';
 
 const ButtonGroup = styled.div`
@@ -22,52 +22,57 @@ interface IPostAddProps {
 const PostAdd: React.SFC<IPostAddProps> = props => {
   const { userId, addPost } = props;
   return (
-    <Formik
-      initialValues={{ title: '', body: '' }}
-      onSubmit={values => {
-        addPost({ ...values, userId });
-      }}
-    >
-      {({ values, handleChange, handleBlur, handleSubmit }) => (
-        <StyledForm onSubmit={handleSubmit} autoComplete="off">
-          <TextField
-            id="title"
-            label="Title"
-            name="title"
-            fullWidth={true}
-            margin="normal"
-            variant="outlined"
-            onChange={handleChange}
-            value={values.title}
-            onBlur={handleBlur}
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-          <TextField
-            id="body"
-            label="Post"
-            name="body"
-            fullWidth={true}
-            margin="normal"
-            variant="outlined"
-            onChange={handleChange}
-            value={values.body}
-            onBlur={handleBlur}
-            multiline={true}
-            rowsMax="4"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-          <ButtonGroup>
-            <Button variant="contained" type="submit" color="primary">
-              Submit
-            </Button>
-          </ButtonGroup>
-        </StyledForm>
-      )}
-    </Formik>
+    <>
+      <Typography variant="h6" align="center">
+        Add your post
+      </Typography>
+      <Formik
+        initialValues={{ title: '', body: '' }}
+        onSubmit={values => {
+          addPost({ ...values, userId });
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <StyledForm onSubmit={handleSubmit} autoComplete="off">
+            <TextField
+              id="title"
+              label="Title"
+              name="title"
+              fullWidth={true}
+              margin="normal"
+              variant="outlined"
+              onChange={handleChange}
+              value={values.title}
+              onBlur={handleBlur}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              id="body"
+              label="Post"
+              name="body"
+              fullWidth={true}
+              margin="normal"
+              variant="outlined"
+              onChange={handleChange}
+              value={values.body}
+              onBlur={handleBlur}
+              multiline={true}
+              rowsMax="4"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <ButtonGroup>
+              <Button variant="contained" type="submit" color="primary">
+                Submit
+              </Button>
+            </ButtonGroup>
+          </StyledForm>
+        )}
+      </Formik>
+    </>
   );
 };
 
